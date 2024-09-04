df1c2f88e25eedc1cb05cf1d683cdcbec457545c
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-04 22:43:22 +0300

    Proceed with creating the header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { FormEvent } from "react";
 import logoImage from "@img/logo.png";
 import {
   Grid2x2,
@@ -10,31 +12,41 @@ import {
   ShoppingCart,
   User,
 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const input = e.currentTarget.search.value;
+    router.push(`/search?q=${input}`);
+    console.log(input);
+  };
   return (
     <header className="flex bg-walmart md:flex-row items-center px-10 py-7 space-x-5 flex-col">
-      <div className="flex space-x-5 w-full items-center">
-        <Link href={"/"}>
-          <Image src={logoImage} alt="logo" width={50} height={50} />
-        </Link>
-        <form
-          action=""
-          className="flex items-center bg-white rounded-full w-full flex-1"
+      <Link href={"/"} className="mb-5 md:mb-0">
+        <Image src={logoImage} alt="logo" width={50} height={50} />
+      </Link>
+      <form
+        action=""
+        className="flex items-center bg-white rounded-full w-full flex-1 text-black"
+        onSubmit={handleSubmit}
+      >
+        <input
+          name="search"
+          type="text"
+          placeholder="Search Everything..."
+          className="flex-1 px-4 rounded-l-full outline-none placeholder:text-xs"
+        />
+        <button
+          type="submit"
+          className="rounded-full size-10 px-2 bg-yellow-400 cursor-pointer"
         >
-          <input
-            type="text"
-            placeholder="Search Everything..."
-            className="flex-1 px-4 rounded-l-full outline-none placeholder:text-xs"
-          />
-          <button
-            type="submit"
-            className="rounded-full size-10 px-2 bg-yellow-400 cursor-pointer"
-          >
-            <Search className="rounded-full h-10" />
-          </button>
-        </form>
-      </div>
+          <Search className="rounded-full h-10" />
+        </button>
+      </form>
+
       <div className="flex space-x-5 mt-5 md:mt-0">
         <Link
           href={"/"}
@@ -72,7 +84,7 @@ const Header = () => {
           </div>
         </Link>
         <Link
-          href={"/"}
+          href={"/basket"}
           className="flex text-white font-bold items-center space-x-2 text-sm"
         >
           <ShoppingCart size={20} />
