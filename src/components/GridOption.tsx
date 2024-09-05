9f0e37adf610c0aaecc47affaf37fc3e17dc7a2c
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-05 18:14:18 +0300

    - Created the GridOption component

---
+++ src/components/GridOption.tsx
@@ -0,0 +1,37 @@
+import { cn } from "@/lib/utils";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const GridOption = ({
+  title,
+  className,
+  image,
+}: {
+  title: string;
+  className?: string;
+  image?: StaticImageData;
+}) => {
+  return (
+    <Link
+      href={{
+        pathname: "/",
+        query: { q: title },
+      }}
+      className={cn("grid-option relative", className)}
+    >
+      <h2>{title}</h2>
+      {image && (
+        <Image
+          src={image}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="opacity-80 rounded-xl"
+        />
+      )}
+    </Link>
+  );
+};
+
+export default GridOption;
