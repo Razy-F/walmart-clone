7f2b844bc1a497ef864a658363e413b33a43c861
Razy F <105067767+Razy-F@users.noreply.github.com>
2024-09-06 22:15:47 +0300

    -Include the main content assets

---
+++ src/images/index.ts
@@ -0,0 +1,23 @@
+import imageOne from "../../public/assets/cover/1.webp";
+import imageTwo from "../../public/assets/cover/2.webp";
+import imageThree from "../../public/assets/cover/3.webp";
+import imageFour from "../../public/assets/cover/4.webp";
+import imageFive from "../../public/assets/cover/5.webp";
+import imageSix from "../../public/assets/cover/6.webp";
+import imageSeven from "../../public/assets/cover/7.webp";
+import imageEight from "../../public/assets/cover/8.jpg";
+import imageNine from "../../public/assets/cover/9.webp";
+import imageTen from "../../public/assets/cover/10.webp";
+
+export {
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
+};
